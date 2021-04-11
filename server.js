f9ce5a55bf5c27b1a88a9fcbab4ed7a3bb7e4749
RoZZZeTkA <rozzztbf@gmail.com>
2021-04-11 22:33:19 +0400

    fix for heroku

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/dist/blog'));
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/blog/index.html'));
 })
 
 console.log('Console listening!');
