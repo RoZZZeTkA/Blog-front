b40b9080e5bf8f3b7345bc5eadf7d815bf215b07
RoZZZeTkA <rozzztbf@gmail.com>
2021-05-16 16:40:47 +0400

    add validators for the registration form

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -12,8 +12,15 @@ import { UserService } from '../user.service';
 })
 export class RegistrationComponent implements OnInit {
 
+  public nickname: string = "";
+  public email: string = "";
   public password: string = "";
   public repeatPassword: string = "";
+  public nicknameValid: boolean = false;
+  public emailValid: boolean = false;
+  public passwordValid: boolean = false;
+  public formValid: boolean = false;
+  public isPasswordStrong: boolean = false;
 
   constructor(private userService: UserService, 
               private jwtClientService: JwtClientService, 
@@ -24,7 +31,7 @@ export class RegistrationComponent implements OnInit {
     this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#f4f4f4';
   }
 
-  public onAddUser(addUserForm: NgForm): void{
+  public onAddUser(addUserForm: NgForm): void {
     console.log(addUserForm.value);
     this.userService.addUsers(addUserForm.value).subscribe(
       (data: User) => {
@@ -37,12 +44,47 @@ export class RegistrationComponent implements OnInit {
     );
   }
 
-  public repeatPasswordChange(): void{
-    if(this.password === this.repeatPassword){
-     (<HTMLInputElement>document.getElementById('repeatPassword')).style.borderLeft = "5px solid #42A948";
-    } else {
-      (<HTMLInputElement>document.getElementById('repeatPassword')).style.borderLeft = "5px solid #A94442";
+  public nicknameValidation(): void {
+    if(this.nickname.length > 0){
+      this.nicknameValid = true;
+      (<HTMLInputElement>document.getElementById('nickname')).style.borderLeft = "5px solid #42A948";
     }
+    else{
+      this.nicknameValid = false;
+      (<HTMLInputElement>document.getElementById('nickname')).style.borderLeft = "5px solid #A94442";
+    }
+    this.formValid = this.nicknameValid && this.emailValid && this.passwordValid;
   }
 
+  public emailValidation(): void {
+    let regEx = new RegExp("^([a-zA-Z0-9])+@([a-z])+\\.([a-z])+$");
+    if(regEx.test(this.email)){
+      this.emailValid = true;
+      (<HTMLInputElement>document.getElementById('email')).style.borderLeft = "5px solid #42A948";
+    }
+    else{
+      this.emailValid = false;
+      (<HTMLInputElement>document.getElementById('email')).style.borderLeft = "5px solid #A94442";
+    }
+    this.formValid = this.nicknameValid && this.emailValid && this.passwordValid;
+  }
+
+  public passwordValidation(): void {
+    let regEx = new RegExp("(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,256}");
+    this.isPasswordStrong = regEx.test(this.password);
+    let isPasswordsEquals = this.password === this.repeatPassword;
+    if(this.isPasswordStrong)
+      (<HTMLInputElement>document.getElementById('password')).style.borderLeft = "5px solid #42A948";
+    else
+      (<HTMLInputElement>document.getElementById('password')).style.borderLeft = "5px solid #A94442";
+    if(isPasswordsEquals)
+      (<HTMLInputElement>document.getElementById('repeatPassword')).style.borderLeft = "5px solid #42A948";
+    else 
+      (<HTMLInputElement>document.getElementById('repeatPassword')).style.borderLeft = "5px solid #A94442";
+    if(this.isPasswordStrong && isPasswordsEquals)
+      this.passwordValid = true;
+    else
+      this.passwordValid = false;
+    this.formValid = this.nicknameValid && this.emailValid && this.passwordValid;
+  }
 }
